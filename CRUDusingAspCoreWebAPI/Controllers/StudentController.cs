162dea04c5f8fa7787dfb2fc1cc0115576cc946e
kushalpatil29 <kushalpatil.2004@gmail.com>
2025-09-02 10:36:31 +0530

    Add project files.

---
+++ CRUDusingAspCoreWebAPI/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using CRUDusingAspCoreWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Threading.Tasks.Dataflow;
+
+namespace CRUDusingAspCoreWebAPI.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string Url = "https://localhost:7093/api/StudentAPI/";
+        private HttpClient client = new HttpClient();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Student> stud = new List<Student>();
+            HttpResponseMessage res = client.GetAsync(Url).Result;
+            if (res.IsSuccessStatusCode)
+            {
+                string result = res.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<List<Student>>(result);
+                if (data != null)
+                {
+                    stud = data;
+                }
+            }
+            
+            return View(stud);
+        }
+    }
+}
---
+++ CRUDusingAspCoreWebAPI/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace CRUDusingAspCoreWebAPI.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string studentName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public int standard { get; set; }
+
+
+    }
+}
